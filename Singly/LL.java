2de46c79cdc271a00df34e3c146e16a094b83922
Rushikesh Rode <rushikeshrode01@gmail.com>
2024-09-07 16:31:38 +0530

    added sll

---
+++ Singly/LL.java
@@ -0,0 +1,177 @@
+package com.rushikesh.LL.Singly;
+
+public class LL {
+
+    Node head;
+    int size;
+
+    public LL(){
+        this.size = 0;
+    }
+
+    public class Node{
+
+        int val;
+        Node next;
+
+        public Node(int val){
+            this.val = val;
+        }
+
+        public Node(int val, Node next){
+            this.val = val;
+            this.next = next;
+        }
+
+    }
+
+    public void insertFirst(int val){
+
+        Node node = new Node(val);
+
+        node.next = head;
+        head = node;
+        size++;
+
+
+    }
+
+    public void display(){
+
+        Node dummy = head;
+
+        if (dummy == null){
+            return;
+        }
+
+        while (dummy != null){
+            System.out.print(dummy.val +"->");
+            dummy = dummy.next;
+        }
+        System.out.println("END");
+    }
+
+    public void insert(int pos, int val){
+
+        Node dummyHead = head;
+
+        if (pos == 0){
+            insertFirst(val);
+            return;
+        }
+
+        for (int i = 0; i < pos-1; i++) {
+            dummyHead = dummyHead.next;
+        }
+
+        if (dummyHead.next == null) {
+            insertLast(val);
+            return;
+        }
+
+        Node node = new Node(val, dummyHead.next);
+        dummyHead.next = node;
+        size++;
+
+    }
+
+    public void insertLast(int val){
+
+        Node dummyHead = head;
+
+        if (dummyHead ==null){
+            insertFirst(val);
+            return;
+        }
+
+        while (dummyHead.next != null){
+            dummyHead = dummyHead.next;
+        }
+
+        Node node = new Node(val);
+        dummyHead.next = node;
+        size++;
+    }
+
+
+    public void deleteFirst(){
+
+        System.out.println(head.val);
+
+        head = head.next;
+
+        size--;
+
+    }
+
+    public void deleteLast(){
+
+        Node dummy = head;
+
+        if (dummy == null)  return;
+
+        while (dummy.next.next != null){
+            dummy = dummy.next;
+        }
+
+        System.out.println(dummy.next.val);
+        dummy.next = null;
+        size--;
+
+    }
+
+    public void delete(int pos){
+
+        if (pos == 0){
+            deleteFirst();
+            return;
+        }
+        if (pos == size-1){
+            deleteLast();
+            return;
+        }
+
+        Node temp = head;
+
+        for (int i = 0; i < pos-1; i++) {
+            temp = temp.next;
+        }
+
+        System.out.println(temp.next.val);
+
+        temp.next = temp.next.next;
+        size--;
+
+    }
+
+    public void peek(int pos){
+
+        Node temp = head;
+
+        for (int i = 0; i < pos; i++) {
+            temp = temp.next;
+        }
+
+        System.out.print("Val of "+pos+" : ");
+        System.out.println(temp.val);
+
+    }
+
+    public void peekFirst(){
+        System.out.print("Head : ");
+        System.out.println(head.val);
+    }
+
+    public void peekLast(){
+
+        Node temp = head;
+        for (int i = 0; i < size-1; i++) {
+            temp = temp.next;
+        }
+        System.out.print("Tail : ");
+        System.out.println(temp.val);
+    }
+
+
+
+}
---
+++ Singly/Main.java
@@ -0,0 +1,36 @@
+package com.rushikesh.LL.Singly;
+
+public class Main {
+    public static void main(String[] args) {
+
+        LL ls = new LL();
+
+        ls.insertLast(12);
+        ls.insertLast(7);
+        ls.insertLast(2);
+        ls.insertLast(3);
+        ls.insertLast(43);
+        ls.insertLast(89);
+        ls.insertLast(6);
+        ls.insert(6,5);
+        ls.insertLast(34);
+        ls.insert(2,54);
+        ls.insert(4, 4);
+
+        ls.display();
+
+        ls.deleteFirst();
+        ls.deleteLast();
+        ls.delete(8);
+
+        ls.display();
+
+        ls.peek(5);
+
+        System.out.println("size : "+ls.size);
+
+        ls.peekFirst();
+        ls.peekLast();
+
+    }
+}
